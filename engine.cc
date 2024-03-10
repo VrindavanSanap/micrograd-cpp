4fa3222805cdccd65b5799075c08a50e4dc66f0d
Vrindavan Sanap <vrindavansanap@gmail.com>
2024-03-11 02:04:01 +0530

    -

--- engine.cc
+++ engine.cc
@@ -1,6 +1,9 @@
+
+#include <functional>
 #include <iostream>
 #include <set>
-
+#include <unordered_map>
+#include <vector>
 using namespace std;
 class Value {
  public:
@@ -8,11 +11,13 @@ class Value {
   float grad;
   vector<Value *> children;
   std::function<void()> _backward;
-
+  vector<Value *> topo;
+  bool has__backward = false;
   Value(float data, vector<Value *> children = vector<Value *>()) {
     this->data = data;
     this->grad = 0;
     this->children = children;
+    this->has__backward = false;
   };
   // __add__ Value + Value
   Value operator+(Value &other) {
@@ -21,13 +26,44 @@ class Value {
     out_children.push_back(&other);
     Value out(this->data + other.data, out_children);
 
-    out._backward = [this, other, out]() mutable {
+    out._backward = [this, other, &out]() mutable {
       this->grad += out.grad;
       other.grad += out.grad;
     };
+    out.has__backward = true;
 
     return out;
+    cout << "plus classed" << endl;
   }
+  Value operator-(Value &other) {
+    vector<Value *> out_children;
+    out_children.push_back(this);
+    out_children.push_back(&other);
+    Value out(this->data - other.data, out_children);
+
+    out._backward = [this, other, &out]() mutable {
+      this->grad -= out.grad;
+      other.grad -= out.grad;
+    };
+    out.has__backward = true;
+
+    return out;
+    cout << "plus classed" << endl;
+  }
+
+  Value operator+(float other) {
+    Value v(other);
+    return this->operator+(v);
+  }
+  Value operator-() { return this->operator*(-1); };
+  // Value operator+=(float other) {
+  //   Value v(other);
+  //   this->operator+(v);
+  // }
+
+  // Value operator+=(Value &other) {
+  //   return this->operator+(other);
+  //   }
   // __mul__ Value + Value
   Value operator*(Value &other) {
     vector<Value *> out_children;
@@ -35,11 +71,53 @@ class Value {
     out_children.push_back(&other);
     Value out(this->data * other.data, out_children);
 
-    out._backward = [this, other, out]() mutable {
-      this->grad += out.grad;
-      other.grad += out.grad;
+    out._backward = [this, other, &out]() mutable {
+      this->grad += other.data * out.grad;
+      other.grad += this->data * out.grad;
+      // cout << other.data * out.grad << ' ' << this->data * out.grad << endl;
     };
+    out.has__backward = true;
+    return out;
+  }
+  Value operator*(float other) {
+    Value v(other);
+    return this->operator*(v);
+  }
 
+  Value operator/(float other) {
+    Value v(other);
+    Value v_inv = v.power(-1);
+    return this->operator*(v_inv);
+  }
+
+  Value power(float other) {
+    vector<Value *> out_children;
+    out_children.push_back(this);
+
+    Value out(std::pow(this->data, other), out_children);
+
+    out._backward = [this, other, &out]() mutable {
+      this->grad += other * std::pow(this->data, other - 1);
+
+      // cout << other.data * out.grad << ' ' << this->data * out.grad << endl;
+    };
+    out.has__backward = true;
+    return out;
+  }
+  Value relu() {
+    vector<Value *> out_children;
+    out_children.push_back(this);
+    float res = 0;
+    if (this->data > 0) {
+      res = this->data;
+    }
+    Value out(res, out_children);
+
+    out._backward = [this, &out]() mutable {
+      this->grad += out.data * (out.grad > 0);
+      // cout << other.data * out.grad << ' ' << this->data * out.grad << endl;
+    };
+    out.has__backward = true;
     return out;
   }
   // __repr__
@@ -47,18 +125,76 @@ class Value {
     os << "Value(data=" << v.data << ")";
     return os;
   }
-  void backward() {
-    vector<Value *> topo;
+  void build_topo(Value &v) {
     set<Value *> visited;
-  };
-};
+    if (visited.find(&v) == visited.end()) {
+      visited.insert(&v);
+    }
+    for (Value *child : v.children) {
+      build_topo(*child);
+    }
+    topo.push_back(&v);
+  }
+  void backward() {
+    build_topo(*this);
+    this->grad = 1;
+    for (Value *valPtr : topo) {
+      if (valPtr != nullptr) {
+        Value val = *valPtr;
+        // cout << val.data;
+        if (val.has__backward) {
+          val._backward();
+        }
 
+      } else {
+        // Handle the case when valPtr is nullptr
+      }
+    }
+  }
+};
+Value operator/(float other, Value &v) {
+  Value other_value(other);
+  Value v_inv = v.power(-1);
+  return other_value * v_inv;
+}
 int main() {
-  Value a(2.0);
-  Value b(3.0);
+  Value a(-4.0);
+  Value b(2.0);
   Value c = a + b;
-  a.data = 10;
-  ;
-  a.backward();
-  cout << (c.children[0]);
+  // a.backward();
+  cout << c << " " << 1 << endl;
+
+  Value b_p_3 = b.power(3);
+  Value d = a * b + b_p_3;
+  cout << d << " " << 2 << endl;
+
+  c = c + c + 1;
+  cout << c << " " << 3 << endl;
+
+  c = c + c + 1 - a;
+  cout << c << " " << 4 << endl;
+  Value t1 = (b + a).relu();
+  d = d * 2 + t1 + d;
+
+  cout << d << " " << 5 << endl;
+  Value t2 = (b - a).relu();
+  d = d * 3 + t2 + d;
+  cout << d << " " << 6 << endl;
+  Value e = c - d;
+
+  cout << e << " " << 7 << endl;
+  Value f = e.power(2);
+  cout << f << " " << 8 << endl;
+  Value g = f / 2.0;
+  cout << g << " " << 9 << endl;
+  Value t3 = 10.0 / f;
+  g = g + t3;
+  cout << g << " " << 10 << endl;
+  // f print(f '{g.data:.4f}') #prints 24.7041, the outcome of this forward pass
+
+  // cout << a.grad << endl;
+  //     print(f '{a.grad:.4f}') #prints 138.8338, i.e.the numerical value of dg
+  //     / da
+  // print(f '{b.grad:.4f}') #prints 645.5773, i.e.the numerical value of dg /
+  // db
 }
